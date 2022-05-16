bbe689d06d8e98db82b5aa15a640be7587cfc54a
PochS <sarah.poch@hotmail.fr>
2022-05-16 11:52:59 +0200

    Update 16.05.2022

---
+++ assignment2/handson_spyder.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import os 
+import sys
+
+#Premier test
+
+print(sys.path)
+help("modules")
+days_of_the_week = 7 
+
+def say_hello(recipient): 
+    print("Hello World ! Hello{}".format(recipient))
